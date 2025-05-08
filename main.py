4a25c31aace155f45d6f5adef6fc865e5295d972
Lzh57 <b0902303389@gmail.com>
2025-05-08 11:36:10 +0800

    第一次上傳

---
+++ main.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, jsonify
+from tensorflow.keras.models import load_model
+from tensorflow.keras.applications.efficientnet import preprocess_input
+import numpy as np
+import cv2
+from PIL import Image
+import io
+import base64
+
+app = Flask(__name__)
+model = load_model("best_model.keras")
+class_names = ["Glass", "Plastic"]
+
+@app.route("/")
+def home():
+    return "AI 分類模型已啟動！"
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    if "image" not in request.files:
+        return jsonify({"error": "缺少圖片"})
+
+    file = request.files["image"]
+    image = Image.open(file.stream).convert("RGB")
+    image = image.resize((299, 299))
+    img_array = np.expand_dims(np.array(image), axis=0).astype(np.float32)
+    img_array = preprocess_input(img_array)
+
+    pred = model.predict(img_array, verbose=0)[0]
+    class_index = np.argmax(pred)
+    confidence = float(pred[class_index])
+    label = class_names[class_index]
+
+    return jsonify({
+        "label": label,
+        "confidence": round(confidence, 3)
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
